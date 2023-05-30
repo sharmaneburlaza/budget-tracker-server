2877f8fea6a1d6b7f1b3a6a09b5a1ad78359aeed
sharmane burlaza <sjmburlaza@gmail.com>
2023-05-31 05:28:49 +0800

    Install packages; set-up index.js

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+dotenv.config();
+
+// parse JSON data in incoming requests
+// allows to access the JSON data sent in the request body within route handlers using req.body
+app.use(bodyParser.json());
+app.use(cors()); // to enable cross-origin resource sharing
+
+mongoose
+    .connect(process.env.MONGO_URL)
+    .then(() => console.log("DBconnection is successful!"))
+    .catch((err) => {
+        console.log(err);
+    });
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+
+app.listen(process.env.PORT || 4000, () => {
+    console.log(`API is now online on port ${ process.env.PORT || 3000 }`)
+});
